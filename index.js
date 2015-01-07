d76cd591a9dd42ced0aac58376fa517ca8ea6ebf
Joe Minichino <joe.minichino@gmail.com>
2015-01-07 14:51:44 +0000

    first import into new repo

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 module.exports = (function () {
-  var FunDB = {};
 
-  function query(coll, fun) {
+  function curry() {
+    var fun = arguments[0],
+      arg = arguments[1];
+    return function () {
+      var args = Array.prototype.slice.call(arguments);
+      args.unshift(arg);
+      return fun.apply(null, args);
+    }
+  }
+
+  function query(fun, coll) {
     return coll.filter(fun);
   }
 
+  function iquery(coll, fun) {
+    return query(fun, coll);
+  }
+
   function andTest(coll, filters) {
-    var result = coll,
+    var result = fork(coll),
       i = 0,
-      len = coll.length;
+      len = filters.length;
 
     for (i; i < len; i += 1) {
-      result = query(result, filters[i]);
+      result = query(filters[i], result);
     }
     return result;
   }
@@ -31,16 +44,24 @@ module.exports = (function () {
   function orTest(coll, filters) {
     var result = [],
       i = 0,
-      len = coll.length;
+      len = filters.length;
+    coll.forEach(function (elem) {
+      for (i = 0; i < len; i += 1) {
+        if (filters[i].apply(null, [elem])) {
+          result.push(JSON.parse(JSON.stringify(elem)));
+          break;
+        }
+      }
+    });
+    return result;
+  };
 
-    for (i; i < len; i += 1) {
-      result.concat(query(coll, filters[i]));
-    }
-    return arrayUnique(result);
+  function reduce(fun, coll) {
+    return coll.reduce(fun);
   }
 
-  function reduce(coll, fun) {
-    return coll.reduce(fun);
+  function ireduce(coll, fun) {
+    return reduce(fun, coll);
   }
 
   function fork(coll) {
@@ -51,9 +72,9 @@ module.exports = (function () {
     obj.$tempIndex = i;
   }
 
-  function index(coll, sortFun) {
+  function index(sortFun, coll) {
     var copy = fork(coll),
-      index = [];;
+      index = [];
     copy.forEach(addTestIndex);
 
     copy.sort(sortFun);
@@ -61,15 +82,36 @@ module.exports = (function () {
       index.push(+elem.$tempIndex);
     });
     delete copy;
-    return index;
+    var i = {};
+    i.ids = index;
+    i.first = function () {
+      return coll[i.ids[0]];
+    };
+    i.last = function () {
+      return coll[i.ids.length - 1];
+    };
+    i.get = function (pos) {
+      return coll[pos];
+    };
+    i.insert = function (item, pos) {
+      coll.push(item);
+      i.ids.splice(pos, 0, coll.length - 1);
+    };
+    return i;
   }
 
-  function addData(data, coll, callback) {
-    coll.push(data);
-    callback();
+  function iindex(coll, sortFun) {
+    return index(sortFun, coll);
   }
 
-  function curryWrite(filename, writeFun) {
+  function addData(coll, obj, callback) {
+    coll.push(obj);
+    if (callback) {
+      callback(coll.length - 1);
+    }
+  }
+
+  function curryWrite(writeFun, filename) {
     return function (callback) {
       writeFun.apply(null, [filename, callback]);
     }
@@ -77,16 +119,47 @@ module.exports = (function () {
 
   function curryData(coll) {
     return function (filter) {
-      return query(coll, filter);
+      return query(filter, coll);
     }
   }
 
   function curryQuery(filter) {
     return function (coll) {
-      return query(coll, filter);
+      return query(filter, coll);
     }
   }
 
+  function searchSorted(coll, index, item, fun) {
+    var lo = 0,
+      hi = coll.length,
+      compared,
+      mid;
+    if (!fun) {
+      fun = function (a, b) {
+        return (a < b) ? -1 : ((a > b) ? 1 : 0);
+      }
+    }
+    while (lo < hi) {
+      mid = ((lo + hi) / 2) | 0;
+      compared = fun(item, coll[index.ids[mid]]);
+      if (compared == 0) {
+        return {
+          found: true,
+          index: mid
+        };
+      } else if (compared < 0) {
+        hi = mid;
+      } else {
+        lo = mid + 1;
+      }
+    }
+    return {
+      found: false,
+      index: hi
+    };
+  }
+
+  var FunDB = {};
   FunDB.query = query;
   FunDB.reduce = reduce;
   FunDB.and = andTest;
@@ -96,6 +169,19 @@ module.exports = (function () {
   FunDB.addData = addData;
   FunDB.curryWrite = curryWrite;
   FunDB.curryQuery = curryQuery;
+  FunDB._ = curry;
+  FunDB.search = searchSorted;
+  FunDB.curry = function (coll) {
+    var db = {};
+    db.query = curryData(coll);
+    db.index = FunDB._(iindex, coll);
+    db.search = FunDB._(searchSorted, coll);
+    db.or = FunDB._(orTest, coll);
+    db.and = FunDB._(andTest, coll);
+    db.reduce = FunDB._(ireduce, coll);
+    db.addData = FunDB._(addData, coll);
+    return db;
+  };
 
   return FunDB;
 
--- test.js
+++ test.js
@@ -1,47 +1,85 @@
+'use strict';
+
 var fdb = require('./index'),
-  msgpack = require('msgpack5')();
+	msgpack = require('msgpack5')();
 
 var data = [{
-  name: 'b',
-  age: 10
+	name: 'joe',
+	age: 63,
+	lang: 'italian'
 }, {
-  name: 'a',
-  age: 20
+	name: 'john',
+	age: 20,
+	lang: 'english'
 }, {
-  name: 'c',
-  age: 30
+	name: 'jack',
+	age: 33,
+	lang: 'english'
 }, {
-  name: 'f',
-  age: 100
+	name: 'jim',
+	age: 10,
+	lang: 'english'
 }, {
-  name: 'g',
-  age: 50
+	name: 'jamess',
+	age: 92,
+	lang: 'italian'
 }];
 
 var FILE = 'test.json',
-  fs = require('fs'),
-  ageIndex = fdb.index(data, function (a, b) {
-    return a.age > b.age;
-  }),
-  ageFilter = fdb.curryQuery(function (obj) {
-    return obj.age > 29;
-  }),
-  writeFile = fdb.curryWrite(FILE, fs.writeFile),
-  writeSuccessful = function (err) {
-    if (err) {
-      console.log('File ' + FILE + ' not saved');
-    }
-    console.log('File ' + FILE + ' saved');
-  },
-  byAge = function (obj) {
-    return obj.age > 30;
-  },
-  byName = function (obj) {
-    return obj.name === 'a';
-  };
+	fs = require('fs'),
+	ageSort = function (a, b) {
+		return a.age > b.age;
+	},
+	ageIndex,
+	ageFilter = fdb.curryQuery(function (obj) {
+		return obj.age > 29;
+	}),
+	writeFile = fdb._(fs.writeFile, FILE),
 
-console.log(ageIndex);
-console.log(ageFilter(data));
-//console.log(fdb.or(data, [byName, byAge]));
+	writeSuccessful = function (err) {
+		if (err) {
+			console.log('File ' + FILE + ' not saved');
+		}
+		console.log('File ' + FILE + ' saved\n\n');
+	},
+	byAge = function (obj) {
+		return obj.age > 30;
+	},
+	byName = function (obj) {
+		return obj.name === 'joe';
+	},
+	byLang = function (obj) {
+		return obj.lang === 'italian';
+	};
+
+console.log('\nCreate an age index\n', ageIndex = fdb.index(ageSort, data));
+console.log('\nFilter by age\n', ageFilter(data));
+console.log('\nDo a complex query with ANDs and ORs\n', fdb.and(fdb.or(data, [byName, byAge]), [byLang]));
+
+
+var collDb = fdb.curry(data);
+
+console.log('Testing curried db\n', collDb.query(byAge));
+console.log('Testing curried index\n', collDb.index(ageSort));
+console.log('Oldest person is ', ageIndex.last());
+
+var newposition;
+/*
+  search returns an object with two properties: found indicates whether the value was found or not, and position 
+  indicates the position at which the item was found or the position it would have if we wanted to insert it.
+*/
+console.log('Search\n', newposition = collDb.search(ageIndex, 39, function (a, b) {
+	return a < b.age ? -1 : (a > b.age ? 1 : 0);
+}));
+ageIndex.insert({
+	name: 'jill',
+	age: 39
+}, newposition.index);
+
+console.log('\nModified array and index\n', data, ageIndex);
+console.log('Search again\n', collDb.search(ageIndex, 39, function (a, b) {
+	return a < b.age ? -1 : (a > b.age ? 1 : 0);
+}));
+
+
 writeFile(msgpack.encode(data), writeSuccessful);
-var f = fs.createReadStream('./test.json').pipe(msgpack.decode);
